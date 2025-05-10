273e51f47848dd506fa6c8986513e43ca883763e
steven-chiu-bigstack <steven.chiu@bigstack.co>
2025-05-10 09:10:13 +0800

    fix: grafana and opensearch links

--- src/apis/cosApi.js
+++ src/apis/cosApi.js
@@ -1,5 +1,36 @@
 import axios from 'axios';
+import { dataCenterStore } from 'src/stores/datacenters/DataCenterStore';
 
 export const cosApiClientV1 = axios.create({
   baseURL: '/cos-api/v1',
+});
+
+let tokenData;
+let expiresAt;
+
+cosApiClientV1.interceptors.request.use(async (config) => {
+  if (config.url.endsWith('/datacenters') || config.url.endsWith('/tokens')) {
+    return config;
+  }
+
+  if (!tokenData || (expiresAt && expiresAt <= new Date())) {
+    const { dataCenter } = dataCenterStore;
+
+    const { data } = (
+      await cosApiClientV1.post(`/datacenters/${dataCenter.name}/tokens`, {
+        name: 'admin',
+        password: 'admin',
+      })
+    ).data;
+
+    tokenData = data;
+
+    const expiresSeconds = tokenData.expires.access ?? 0;
+    expiresAt = new Date();
+    expiresAt.setSeconds(expiresAt.getSeconds() + expiresSeconds);
+  }
+
+  config.headers.Authorization = `Bearer ${tokenData.token}`;
+
+  return config;
 });
